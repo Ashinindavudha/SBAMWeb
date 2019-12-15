addbc5799ec191678eb8237ae4a6115ed7e00ce9
Kyi Soe Lynn <kyisoelynn@gmail.com>
2019-12-15 12:57:17 +0630

    foreign key column fixed

--- database/migrations/2019_12_11_025741_create_category_posts_table.php
+++ database/migrations/2019_12_11_025741_create_category_posts_table.php
@@ -15,13 +15,15 @@ class CreateCategoryPostsTable extends Migration
     {
         Schema::create('category_posts', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('post_id')->unsigned()->index();
-            $table->integer('category_id')->unsigned()->index();
+            $table->unsignedBigInteger('post_id'); //You must use unsignedBigInteger for foreign key column. integer is not ok.
+            $table->unsignedBigInteger('category_id');
             $table->timestamps();
             /*$table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');*/
 
-            
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
 
         });
     }
--- database/migrations/2019_12_11_025922_create_post_tags_table.php
+++ database/migrations/2019_12_11_025922_create_post_tags_table.php
@@ -15,15 +15,16 @@ class CreatePostTagsTable extends Migration
     {
         Schema::create('post_tags', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('post_id')->unsigned()->index();
-            $table->integer('tag_id')->unsigned()->index();
-            //$table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
-            //$table->foreign('post_id')->references('id')->on('posts')
-            //->onDelete('cascade');
+            $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('tag_id');
+            $table->softDeletes();
             $table->timestamps();
+
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
             
             
-            //$table->softDeletes();
+            
         });
 
     }
